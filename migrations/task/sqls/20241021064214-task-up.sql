d5c22396d730d2f698a4c1e55f531fa2a59b0112
YenYooo <point211@gmail.com>
2024-11-28 22:27:39 +0800

    Update 1-5

--- migrations/task/sqls/20241021064214-task-up.sql
+++ migrations/task/sqls/20241021064214-task-up.sql
@@ -35,6 +35,9 @@ WHERE email = 'opacity0@hexschooltest.io';
 SELECT COUNT(*) FROM "USER" ;
 
 -- 1-5 查詢：取得 USER 資料表所有用戶資料，並列出前 3 筆（提示：使用limit語法）
+SELECT * 
+FROM "USER" 
+LIMIT 3;
 
 
 --  ████████  █████   █    ████  
