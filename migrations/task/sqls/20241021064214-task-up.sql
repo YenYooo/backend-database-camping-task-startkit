2f69e5ca4cc53cc3096f21662636b833fa070fc1
YenYooo <point211@gmail.com>
2024-11-29 03:08:13 +0800

    Update 5-8 challenge question

--- migrations/task/sqls/20241021064214-task-up.sql
+++ migrations/task/sqls/20241021064214-task-up.sql
@@ -274,7 +274,26 @@ GROUP BY "COURSE_BOOKING" .user_id;
     -- from ( 用戶王小明的購買堂數 ) as "CREDIT_PURCHASE"
     -- inner join ( 用戶王小明的已使用堂數) as "COURSE_BOOKING"
     -- on "COURSE_BOOKING".user_id = "CREDIT_PURCHASE".user_id;
-
+SELECT 
+	"CREDIT_PURCHASE".user_id AS user_id ,
+	 ("CREDIT_PURCHASE".total_credit - "COURSE_BOOKING".used_credit) as remaining_credit -- 用戶王小明的剩餘可用堂數
+FROM
+	(SELECT 
+		user_id,
+		SUM(purchased_credits) AS total_credit 
+	FROM "CREDIT_PURCHASE" 
+	WHERE user_id = (SELECT id FROM "USER" WHERE email = 'wXlTq@hexschooltest.io')
+	GROUP BY user_id
+	) AS "CREDIT_PURCHASE" -- 用戶王小明的購買堂數 
+INNER JOIN 
+	(SELECT 
+		user_id,
+		COUNT(*) AS used_credit
+	FROM "COURSE_BOOKING" 
+	WHERE user_id = (SELECT id FROM "USER" WHERE email = 'wXlTq@hexschooltest.io') AND cancelled_at IS NULL
+	GROUP BY user_id
+	) AS "COURSE_BOOKING" -- 用戶王小明的已使用堂數
+ON"COURSE_BOOKING".user_id = "CREDIT_PURCHASE".user_id;
 
 -- ████████  █████   █     ███  
 --   █ █   ██    █  █     █     
